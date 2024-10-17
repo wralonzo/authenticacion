1ca66f75d39258d0898c53a906772d584bbc3e61
William Alonzo <williamalonzo4519@gmail.com>
2024-10-16 19:51:28 -0600

    Se agrego envio de correos

--- app/Controllers/EmailController.php
+++ app/Controllers/EmailController.php
@@ -3,15 +3,27 @@
 namespace App\Controllers;
 
 use CodeIgniter\Controller;
+use App\Models\UsersModel;
 
-class EmailController extends Controller {
+class EmailController extends Controller
+{
 
-    public function send_email() {
+    public function send_email()
+    {
         $email = \Config\Services::email();
+        $userModel = new UsersModel();
+        $users = $userModel->findAll();
+
+        $emailAddresses = [];
+
+        // Extraer los correos de los usuarios
+        foreach ($users as $user) {
+            $emailAddresses[] = $user['email']; // Asumiendo que el campo de email se llama 'email' en la tabla de usuarios
+        }
 
         // Configurar los detalles del correo
         $email->setFrom('invoicerenergy@gmail.com', '');
-        $email->setTo('test@gmail.com');
+        $email->setTo($emailAddresses); 
         $email->setSubject('Automatizacion recetas');
         $email->setMessage('<p>Este es un mensaje de prueba enviado desde CodeIgniter 4 usando Gmail SMTP.</p>');
 
